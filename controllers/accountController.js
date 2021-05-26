6e104d5fef81e8d6751bd902e7b6a018caa68ca8
prayz212 <diecluchivi2701@gmail.com>
2021-05-26 13:35:33 +0700

    commit assignment 2

--- controllers/accountController.js
+++ controllers/accountController.js
@@ -1,11 +1,13 @@
 const Account = require('../models/accountModel')
 const bcrypt = require('bcryptjs')
-const saltRounds = 12
+const jwt = require('jsonwebtoken')
+const PRIVATE_KEY = process.env.PRIVATE_KEY
+const SALT_ROUNDS = 12
 
 exports.handleRegistration = (req, res) => {
     let {username, password, fullname, id, age, title, email} = req.body
 
-    bcrypt.genSalt(saltRounds)
+    bcrypt.genSalt(SALT_ROUNDS)
     .then(salt => {
         bcrypt.hash(password, salt)
         .then(hashed => {
@@ -26,10 +28,58 @@ exports.handleRegistration = (req, res) => {
         })
     })
     .catch(err => {
-        
+        return res.json({code: 400, status: "failed", error: err.message})
     })
 }
 
 exports.handleLogin = (req, res) => {
     let {username, password} = req.body
+
+    Account.find({username})
+    .then(account => {  
+        if (account.length <= 0) {
+            throw new Error("username or password is not correct.")
+        }
+        return account[0]
+    })
+    .then(user => {
+        if (user) {
+            bcrypt.compare(password, user.password)
+            .then(match => { //return TRUE if match otherwise is FALSE
+                if (match) {
+                    const token = jwt.sign({data: user._id}, PRIVATE_KEY, {expiresIn: 10*60})
+                    return res.json({code: 200, status: "successed", token})
+                }
+                else if (!match) {
+                    throw new Error("username or password is not correct.")
+                }
+            })
+            .catch(err => {
+                return res.json({code: 404, status: "failed", error: err.message})
+            })
+        }
+    })
+    .catch(err => {
+        return res.json({code: 404, status: "failed", error: err.message})
+    })
+}
+
+exports.handleGetUserInfo = (req, res) => {
+    let {token} = req.body
+
+    jwt.verify(token, PRIVATE_KEY, function(err, payload) {
+        if (err) return res.json({code: 404, status: "failed", error: err.message})
+        else {
+            let id = payload.data
+
+            Account.findById(id, '-_id -__v -password')
+            .then(result => {
+                return res.json({code: 200, status: "successed", data: result})        
+            })
+            .catch(err => {
+                return res.json({code: 404, status: "failed", error: err.message})        
+            })
+        }
+        
+    })
 }
--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -4,5 +4,6 @@ const accountController = require('../controllers/accountController')
 
 router.post('/register', accountController.handleRegistration)
 router.post('/login', accountController.handleLogin)
+router.post('/getInfo', accountController.handleGetUserInfo)
 
 module.exports = router
